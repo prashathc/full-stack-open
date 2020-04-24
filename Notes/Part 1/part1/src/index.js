039983c374fbf741a8c5a1122b586d7abbb2bb7b
prashathc <prashathc@gmail.com>
2020-04-24 10:19:22 -0400

    done most of exercise 2

--- Notes/Part 1/part1/src/index.js
+++ Notes/Part 1/part1/src/index.js
@@ -14,19 +14,44 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+
+  const handleGood = () => {
+      setGood(good + 1)
+      setCounter(counter + 1)
+  }
+
+  const handleNeutral = () => {
+      setNeutral(neutral + 1)
+      setCounter(counter + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setCounter(counter + 1)
+  }
+
+  
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={()=>{setGood(good + 1)}} text={"good"}></Button>
-      <Button onClick={()=>{setNeutral(neutral + 1)}} text={"neutral"}></Button>
-      <Button onClick={()=>{setBad(bad + 1)}} text={"bad"}></Button>
+      <Button onClick={handleGood} text={"good"}></Button>
+      <Button onClick={handleNeutral} text={"neutral"}></Button>
+      <Button onClick={handleBad} text={"bad"}></Button>
 
       <h1>statistics</h1>
 
-      <Stats text={"good"} score={good}></Stats>
-      <Stats text={"neutral"} score={neutral}></Stats>
-      <Stats text={"bad"} score={bad}></Stats>
+      {counter == 0 ? <p>No feedback given</p> 
+      : <>
+        <Stats text={"good"} score={good}/>
+        <Stats text={"neutral"} score={neutral}/>
+        <Stats text={"bad"} score={bad}/>
+        <Stats text={"all"} score={counter}/>
+        </>
+      }
+
     </div>
   )
 }
@@ -34,3 +59,4 @@ const App = () => {
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
+
